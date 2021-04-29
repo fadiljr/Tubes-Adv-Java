a3bffdb087365eef2895db1a73ce7b732e954d04
fadiljr <fadiljanuar44@gmail.com>
2021-04-30 00:03:49 +0700

    update app

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/users", userRouter);
 
-app.all("*", (req, res, nest) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
